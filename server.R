1184d82bdf8c61b7e6bfac2c52d4480785362442
Francisco Charte Ojeda <francisco@fcharte.com>
2015-02-12 17:39:07 +0100

    Added user authentication and rate limits

--- server.R
+++ server.R
@@ -1,6 +1,26 @@
 library(shiny)
+library(jsonlite)
+library(httr)
 
 shinyServer(function(input, output) {
+  user <- ""
+  password <- ""
+
+  request <- function(url) {
+    user <- input$user
+    password <- input$password
+    if(user != "" && password != "")
+      GET(url, authenticate(user, password, type = 'basic'))
+    else
+      GET(url)
+  }
+
+  limits <- reactive({
+    limits <- request("https://api.github.com/rate_limit")
+    data.frame(unlist(fromJSON(content(limits, "text"))))
+  })
+  output$limits <- renderTable(limits())
+
   output$Rinfo <- renderTable({
     data.frame(unlist(version))
   })
--- ui.R
+++ ui.R
@@ -5,6 +5,13 @@ shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
+      h3("API rate limits"),
+      tableOutput("limits"),
+      actionButton("update", "Update"),
+      hr(),
+      h3("Github user account"),
+      textInput("user", label = h5("User name")),
+      textInput("password", label = h5("Password"))
     ),
 
     mainPanel(
