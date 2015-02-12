309c2f0eb7ca5fb0c378680101d89c31cec6efdc
Francisco Charte Ojeda <francisco@fcharte.com>
2015-02-12 17:58:23 +0100

    Update limits only after click Update button

--- server.R
+++ server.R
@@ -3,23 +3,24 @@ library(jsonlite)
 library(httr)
 
 shinyServer(function(input, output) {
-  user <- ""
-  password <- ""
-
-  request <- function(url) {
-    user <- input$user
-    password <- input$password
+  request <- function(url, user, password) {
     if(user != "" && password != "")
       GET(url, authenticate(user, password, type = 'basic'))
     else
       GET(url)
   }
 
-  limits <- reactive({
-    limits <- request("https://api.github.com/rate_limit")
-    data.frame(unlist(fromJSON(content(limits, "text"))))
+  observe({
+    if(input$update != 0) isolate({
+      user <- input$user
+      password <- input$password
+      limits <- reactive({
+        limits <- request("https://api.github.com/rate_limit", user, password)
+        data.frame(unlist(fromJSON(content(limits, "text"))))
+      })
+      output$limits <- renderTable(limits())
+    })
   })
-  output$limits <- renderTable(limits())
 
   output$Rinfo <- renderTable({
     data.frame(unlist(version))
