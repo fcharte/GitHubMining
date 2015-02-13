6a8d2e7883fa40f79d47a2607f4f8a9ba13112b6
Francisco Charte Ojeda <francisco@fcharte.com>
2015-02-13 08:19:40 +0100

    Added Intro page to UI

--- ui.R
+++ ui.R
@@ -14,9 +14,19 @@ shinyUI(fluidPage(
       h5("Password"),
       tags$input(id="password", type="password", placeholder="Password", class="form-control shiny-bound-input")
     ),
-
     mainPanel(
-      tableOutput("Rinfo")
+      tabsetPanel(id = "pages", type = "pills", selected = "Intro",
+                  tabPanel("Intro", fluidPage(
+                    h3("Introduction to GitHubMining", style="color: blue"),
+                    p("This application allows you to access the GitHub API to get data about users, repositories, contributions, etc."),
+                    p("Use the buttons above this text to access the sections of the application."),
+                    h4("Rate limits accessing GitHub API", style="color: blue"),
+                    p("GitHub limits the number of calls a user can send to its API. Use the ", strong("Update"),
+                      " button, at the left, to see your current limits."),
+                    p("More restrictive limits are applied to anonymous users than to authenticated users. So, to extend de number of calls you can make please introduce your GitHub user name and password. These credentials will be used only for accessing the GitHub API, they are not sent anywhere.")
+                    )),
+                  tabPanel("R Info", fluidPage(tableOutput("Rinfo")))
+      )
     )
   )
 ))
